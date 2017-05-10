d6e1715ca93cb6cca551dd48fde8d20607053739
Devin Etcitty <devin.etcitty@gmail.com>
2017-05-10 15:13:02 +0000

    Problem 3 of hw1

--- homework/homework_1/problem_3/eccentricity.R
+++ homework/homework_1/problem_3/eccentricity.R
@@ -4,3 +4,34 @@ library(scales)
 ########################################
 # YOUR SOLUTION BELOW
 ########################################
+
+
+setwd("~/columbia/APMA4990/msd-homework/homework/homework_1/problem_3")
+
+ratings <- read.table("ratings.csv", header=FALSE, sep=",")
+
+head(ratings)
+
+tail(ratings)
+
+colnames(ratings) <- c("UserID", "MovieID", "Rating", "Timestamp")
+
+head(ratings)
+
+tail(ratings)
+
+ratings %>% 
+  group_by(MovieID) %>%
+  summarise((sum(UserID))) %>%
+  ungroup()
+
+
+
+new <- count(ratings, UserID)
+
+colnames(new) <- c("UserID", "Count")
+
+filter(new, Count > 10 & Count < 30)
+
+movies <- read_table("movies.csv", header=FALSE, sep=",")
+
